fe5278c2123b8d0989ea017472e1e501c7568215
jntk.hong <jthonga2@gmail.com>
2023-11-25 23:37:23 +0900

    입자들의 이동반경을 더 작고 견고하게 만들기

--- attractor.js
+++ attractor.js
@@ -2,7 +2,7 @@ class Attractor {
   constructor(x, y) {
     this.position = createVector(width/2, height/2);
     //{!1} How strong is the repeller?
-    this.power = 150;
+    this.power = 1000;
   }
   
   move(value) {
@@ -16,15 +16,15 @@ class Attractor {
   show() {
     stroke(0);
     strokeWeight(2);
-    fill(200, 130, 50);
-    circle(width/2, height/2, 32);
+    fill(80, 120, 150);
+    circle(width/2, height/2, 25);
   }
 
   pull(particle) {
     //{!6 .code-wide} This is the same repel algorithm we used in Chapter 2: forces based on gravitational attraction.
     let force = p5.Vector.sub(this.position, particle.position);
     let distance = force.mag();
-    distance = constrain(distance, 5, 50);
+    distance = constrain(distance, 10, 11);
     let strength = this.power / (distance * distance);
     force.setMag(strength);
     return force;
