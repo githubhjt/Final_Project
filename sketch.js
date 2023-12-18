b0a0f7a9c0db5efa2058b7e75072b89fe3aaf77f
jntk.hong <jthonga2@gmail.com>
2023-12-18 21:04:20 +0900

    이 부분을 주석처리하니까 기존 코드가 다시 실행은 됨. 이부분이 문제인가

--- sketch.js
+++ sketch.js
@@ -92,12 +92,12 @@ function draw() {
   beginShape();
   let head = springparticles[0];
   curveVertex(head.position.x, head.position.y);
-  for (let p of springparticles) {
-    p.applyForce(gravity);
-    p.update();
-    curveVertex(p.position.x, p.position.y);
-    //p.show();
-  }
+  // for (let p of springparticles) {
+  //   p.applyForce(gravity);
+  //   p.update();
+  //   curveVertex(p.position.x, p.position.y);
+  //   // p.show();
+  // }
   let tail = springparticles[springparticles.length - 1];
   curveVertex(tail.position.x, tail.position.y);
   endShape();
